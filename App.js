4cdacb068712b1b070de0af6cb17824bc0683a2a
Leonard <leonardwkee@gmail.com>
2021-12-22 22:54:27 -0800

    part 12.1 - 12.13: adding initial functionality to add blog post

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { createAppContainer } from 'react-navigation'
+import { createStackNavigator } from 'react-navigation-stack'
+import { BlogProvider } from './src/context/BlogContext'
+import IndexScreen from './src/screens/IndexScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
   },
-});
+  {
+    initialRouteName: 'Index',
+    defaultNavigationOptions: {
+      title: 'Blog',
+    },
+  }
+)
+
+const App = createAppContainer(navigator)
+
+export default () => (
+  <BlogProvider>
+    <App />
+  </BlogProvider>
+)
+
