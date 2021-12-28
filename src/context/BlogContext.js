1dd13df262bcee22459f08cc266dcc9747da0170
Leonard <leonardwkee@gmail.com>
2021-12-27 19:51:52 -0800

    add calls to server

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,10 @@
 import createDataContext from './createDataContext'
+import jsonServer from '../api/jsonServer'
 
 const blogReducer = (state, action) => {
   switch (action.type) {
-    case 'add_blogpost':
-      return [
-        ...state,
-        {
-          id: Math.floor(Math.random() * 9999999),
-          title: action.payload.title,
-          content: action.payload.content
-        }
-      ]
+    case 'get_blogposts':
+      return action.payload
     case 'delete_blogpost':
       return state.filter((blogPost) => blogPost.id !== action.payload)
     case 'edit_blogpost':
@@ -20,24 +14,35 @@ const blogReducer = (state, action) => {
   }
 }
 
-const addBlogPost = (dispatch) => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } })
+const getBlogPosts = (dispatch) => {
+  return async () => {
+    const response = await jsonServer.get('/blogposts')
+
+    dispatch({ type: 'get_blogposts', payload: response.data })
+  }
+}
+
+const addBlogPost = () => {
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', { title, content })
     callback && callback()
   }
 }
 
 const deleteBlogPost = (dispatch) => {
-  return (id) => {
+  return async (id) => {
+    await jsonServer.delete(`/blogposts/${id}`)
     dispatch({ type: 'delete_blogpost', payload: id })
   }
 }
 
 const editBlogPost = (dispatch) => {
-  return (id, title, content, callback) => {
+  return async (id, title, content, callback) => {
+    await jsonServer.put(`/blogposts/${id}`, { title, content })
+
     dispatch({ type: 'edit_blogpost', payload: { id, title, content } })
     callback && callback()
   }
 }
 
-export const { Context, Provider } = createDataContext(blogReducer, { addBlogPost, deleteBlogPost, editBlogPost }, [])
+export const { Context, Provider } = createDataContext(blogReducer, { getBlogPosts, addBlogPost, deleteBlogPost, editBlogPost }, [])
